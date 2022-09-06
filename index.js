abd9cf19af00fb71a82a39d0b50d8461acc924c9
itcorner21 <bdjaml99796@gmail.com>
2022-09-06 04:28:49 -0700

    add js code

---
+++ index.js
@@ -0,0 +1,56 @@
+// var eggPrice = 40;
+// var name = "jamal";
+// var isRaning = true;
+// var fastName;
+// var price = 30.99;
+// console.log( typeof eggPrice);
+// console.log(typeof name);
+// console.log(typeof isRaning);
+// console.log(typeof fastName);
+// console.log(typeof price);
+
+// // // eggPrice = eggPrice + 20;
+
+// // eggPrice -= 20;
+
+// // var love = 90;
+// // var love = love+10;
+// // love = love + 10;
+// // love += 10;
+// // love++;
+
+// // console.log(love);
+
+
+
+// concatenation
+var fastName = 'sahan';
+var lastName = 'miah';
+var fullName = fastName +' '+ lastName;
+// console.log(fullName);
+
+
+// conversion
+
+var eggPrice = "40";
+var eggPrice = parseInt(eggPrice);
+console.log(eggPrice);
+
+var price = "99.99";
+var price = parseFloat(price);
+console.log(price);
+
+var a = 0.1;
+var b = 0.2;
+var total = a+b;
+console.log(parseFloat(total.toFixed(1)));
+
+
+// () function
+// {} Object
+// [] array
+
+
+
+
+
